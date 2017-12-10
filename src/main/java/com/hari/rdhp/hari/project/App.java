187a10ed37f7165d519bb1ad701cd70e9373f309
Hariramasamy <hari.csesoft@gmail.com>
2017-12-10 22:01:00 +0530

    Initial commit

---
+++ src/main/java/com/hari/rdhp/hari/project/App.java
@@ -0,0 +1,20 @@
+package com.hari.rdhp.hari.project;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+//@slf4j
+public class App
+{
+    public static void main( String[] args )
+    {
+    	Logger log=LoggerFactory.getLogger(App.class);
+    	log.info("App Starts");
+    	log.info("App Ends");
+    }
+
+}
---
+++ src/main/java/com/hari/rdhp/hari/project/slf4j.java
@@ -0,0 +1,16 @@
+package com.hari.rdhp.hari.project;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface slf4j {
+	Logger log = LoggerFactory.getLogger(App.class);
+
+}
