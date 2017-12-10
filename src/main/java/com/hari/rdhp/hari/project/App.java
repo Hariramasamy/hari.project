6cb99c551e109c98ed0676b6533ecd2894c27df2
Hariramasamy <hari.csesoft@gmail.com>
2017-12-10 22:41:55 +0530

    first change in local

--- src/main/java/com/hari/rdhp/hari/project/App.java
+++ src/main/java/com/hari/rdhp/hari/project/App.java
@@ -1,5 +1,8 @@
 package com.hari.rdhp.hari.project;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,7 +17,17 @@ public class App
     {
     	Logger log=LoggerFactory.getLogger(App.class);
     	log.info("App Starts");
+		Scanner scanInput = new Scanner(System.in);
+    	int inputInt = scanInput.nextInt();
+    	log.info("The Entered Input is "+getResult(inputInt)); 
     	log.info("App Ends");
+    }
+    
+    private static int getResult(int result) {
+    	if(Objects.nonNull(result)) {
+    		return result;
+    	}
+    	return 0;
     }
 
 }
